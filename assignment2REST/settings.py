2b38b276497fb8747ca73bb50ec9b06a3bb74b4f
ClientHenry <wanghao0628@hotmail.com>
2024-06-21 02:02:45 +1200

    vercel new problem

--- assignment2REST/settings.py
+++ assignment2REST/settings.py
@@ -143,6 +143,7 @@ CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` wil
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
 	'http://localhost:3000',
+	"https://web-assignment-2-react.vercel.app",
 ]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
 	'http://localhost:3000',
