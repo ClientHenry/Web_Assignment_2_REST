3399a5c3f26aca648b02965b2af74cd4d21f9f76
ClientHenry <wanghao0628@hotmail.com>
2024-06-22 13:46:37 +1200

    email

--- rest/views.py
+++ rest/views.py
@@ -1,21 +1,28 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.core.mail import send_mail
+
+from rest.models import Class
 from rest.permissions import IsLecturer
+from rest.serializers import StudentEnrollmentSerializer
 
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated, IsLecturer])
-def send_email_to_class(request):
-	try:
-		send_mail(
-			'Your Grade is Ready',
-			'Your Grade is Ready',
-			'wanghao0628@hotmail.com',
-			['wangh159@myunitec.ac.nz']
-		)
-		return Response({'success': 'Email sent successfully'}, status=200)
-	except Exception as e:
-		return Response({'error': str(e)}, status=500)
+def send_email_to_class(request, pk):
+	selected_class = get_object_or_404(Class, id=pk)
+	enrollments = selected_class.enrollments.all()
+	StudentEnrollmentSerializer(enrollments, many=True)
+
+	for enrollment in enrollments:
+		student_name = enrollment.studentID
+		subject = 'Your Grade is Ready'
+		body = f'Hello {student_name}, Your Grade is Ready'
+		from_email = 'wanghao0628@hotmail.com'
+		to_email = enrollment.studentID.email
+		send_mail(subject, body, from_email, [to_email])
+
+	return Response({'success': 'Email sent successfully'}, status=200)
