d61dba6fc3d39944288d8232110530bf6c25504a
ClientHenry <wanghao0628@hotmail.com>
2024-06-22 12:39:39 +1200

    email

--- rest/views.py
+++ rest/views.py
@@ -3,10 +3,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.core.mail import send_mail
+from rest.permissions import IsLecturer
 
 
 @api_view(['POST'])
-# @permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated, IsLecturer])
 def send_email_to_class(request):
 	try:
 		send_mail(
